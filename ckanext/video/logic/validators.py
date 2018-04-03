5fbc3441f8ef5809dec6313676ed649d1813c4e7
Alice Butcher <alycejenni@gmail.com>
2018-04-03 11:57:01 +0100

    auto corrections

--- ckanext/video/logic/validators.py
+++ ckanext/video/logic/validators.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-Created by 'bens3' on 2013-06-21.
-Copyright (c) 2013 'bens3'. All rights reserved.
-"""
+#
+# This file is part of ckanext-video
+# Created by the Natural History Museum in London, UK
+
 import re
 from ckan.common import _
 import ckan.lib.navl.dictization_functions as df
@@ -16,9 +16,14 @@ Missing = df.Missing
 
 def is_valid_video_url(value, context):
 
-    """Validate a URL is a valid video URL"""
+    '''Validate a URL is a valid video URL
+
+    :param value: 
+    :param context: 
+
+    '''
     for pattern in video_provider_patterns.values():
         if re.search(pattern, value, re.IGNORECASE):
             return value
 
-    raise Invalid(_('URL is not a valid video provider'))
+    raise Invalid(_(u'URL is not a valid video provider'))
--- ckanext/video/tests/test_view.py
+++ ckanext/video/tests/test_view.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-video
+# Created by the Natural History Museum in London, UK
+
 import paste.fixture
 import pylons.config as config
 import urlparse
@@ -12,55 +18,61 @@ import ckan.config.middleware as middleware
 
 
 class TestVideoView(tests.WsgiAppCase):
+    ''' '''
 
     @classmethod
     def setup_class(cls):
+        ''' '''
 
-        cls.config_templates = config['ckan.legacy_templates']
-        config['ckan.legacy_templates'] = 'false'
-        wsgiapp = middleware.make_app(config['global_conf'], **config)
+        cls.config_templates = config[u'ckan.legacy_templates']
+        config[u'ckan.legacy_templates'] = u'false'
+        wsgiapp = middleware.make_app(config[u'global_conf'], **config)
         cls.app = paste.fixture.TestApp(wsgiapp)
 
-        plugins.load('video_view')
+        plugins.load(u'video_view')
 
         create_test_data.CreateTestData.create()
 
-        context = {'model': model,
-                   'session': model.Session,
-                   'user': model.User.get('testsysadmin').name}
+        context = {u'model': model,
+                   u'session': model.Session,
+                   u'user': model.User.get(u'testsysadmin').name}
         #
-        cls.package = model.Package.get('annakarenina')
+        cls.package = model.Package.get(u'annakarenina')
         cls.resource_id = cls.package.resources[1].id
         cls.resource_view = {
-            'resource_id': cls.resource_id,
-            'view_type': 'video',
-            'title': u'Test View',
-            'description': u'A nice test view',
-            'height': 400,
-            'width': 400,
-            'video_url': 'https://www.youtube.com/embed/-voP-V7YcqA'
+            u'resource_id': cls.resource_id,
+            u'view_type': u'video',
+            u'title': u'Test View',
+            u'description': u'A nice test view',
+            u'height': 400,
+            u'width': 400,
+            u'video_url': u'https://www.youtube.com/embed/-voP-V7YcqA'
         }
-        cls.resource_view = plugins.toolkit.get_action('resource_view_create')(context, cls.resource_view)
+        cls.resource_view = plugins.toolkit.get_action(u'resource_view_create')(context, cls.resource_view)
 
     @classmethod
     def teardown_class(cls):
-        config['ckan.legacy_templates'] = cls.config_templates
+        ''' '''
+        config[u'ckan.legacy_templates'] = cls.config_templates
         model.repo.rebuild_db()
 
     def test_video_is_shown(self):
-        url = h.url_for(controller='package', action='resource_read', id=self.package.name, resource_id=self.resource_id)
+        ''' '''
+        url = h.url_for(controller=u'package', action=u'resource_read', id=self.package.name, resource_id=self.resource_id)
         result = self.app.get(url)
-        assert self.resource_view['video_url'] in result
+        assert self.resource_view[u'video_url'] in result
 
     def test_title_description_iframe_shown(self):
-        url = h.url_for(controller='package', action='resource_read', id=self.package.name, resource_id=self.resource_id)
+        ''' '''
+        url = h.url_for(controller=u'package', action=u'resource_read', id=self.package.name, resource_id=self.resource_id)
         result = self.app.get(url)
-        assert self.resource_view['title'] in result
-        assert self.resource_view['description'] in result
-        assert 'iframe' in result.body
+        assert self.resource_view[u'title'] in result
+        assert self.resource_view[u'description'] in result
+        assert u'iframe' in result.body
 
     def test_iframe_attributes(self):
-        url = h.url_for(controller='package', action='resource_read', id=self.package.name, resource_id=self.resource_id)
+        ''' '''
+        url = h.url_for(controller=u'package', action=u'resource_read', id=self.package.name, resource_id=self.resource_id)
         result = self.app.get(url)
-        assert 'width="%s"' % self.resource_view['width'] in result
-        assert 'height="%s"' % self.resource_view['height'] in result
+        assert u'width="%s"' % self.resource_view[u'width'] in result
+        assert u'height="%s"' % self.resource_view[u'height'] in result
