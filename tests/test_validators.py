063cff2f8b702b5c091353780a99291c914a802d
Ginger Butcher <alycejenni@gmail.com>
2022-11-15 12:56:28 +0000

    style: apply formatting

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -5,9 +5,10 @@ from mock import MagicMock
 
 
 class TestValidVideoURL(object):
-
     def test_vimeo_valid(self):
-        assert is_valid_video_url('https://player.vimeo.com/video/459651092', MagicMock())
+        assert is_valid_video_url(
+            'https://player.vimeo.com/video/459651092', MagicMock()
+        )
         assert is_valid_video_url('//player.vimeo.com/video/459651092', MagicMock())
 
     def test_vimeo_invalid(self):
@@ -17,7 +18,9 @@ class TestValidVideoURL(object):
             is_valid_video_url('https://vimeo.com/459651092', MagicMock())
 
     def test_youtube_valid(self):
-        assert is_valid_video_url('https://www.youtube.com/watch?v=-voP-V7YcqA', MagicMock())
+        assert is_valid_video_url(
+            'https://www.youtube.com/watch?v=-voP-V7YcqA', MagicMock()
+        )
 
     def test_youtube_invalid(self):
         with pytest.raises(toolkit.Invalid):
@@ -26,7 +29,9 @@ class TestValidVideoURL(object):
             is_valid_video_url('https://www.youtube.com/watch?v=', MagicMock())
 
     def test_youtube_embed_valid(self):
-        assert is_valid_video_url('https://www.youtube.com/embed/-voP-V7YcqA', MagicMock())
+        assert is_valid_video_url(
+            'https://www.youtube.com/embed/-voP-V7YcqA', MagicMock()
+        )
 
     def test_youtube_embed_invalid(self):
         with pytest.raises(toolkit.Invalid):
